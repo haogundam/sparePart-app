72c5f239f866d2983773ffc951264e7457e30053
JosephJan05 <152236459+JosephJan05@users.noreply.github.com>
2023-12-13 14:18:52 +0800

    Joseph

---
+++ src/app/quotation-sidebar/quotation-sidebar.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QuotationSidebarComponent } from './quotation-sidebar.component';
+
+describe('QuotationSidebarComponent', () => {
+  let component: QuotationSidebarComponent;
+  let fixture: ComponentFixture<QuotationSidebarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [QuotationSidebarComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(QuotationSidebarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
